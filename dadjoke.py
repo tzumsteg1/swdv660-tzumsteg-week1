e9189d83c5dd19aaa1d54967ee6fac967c8d8b5f
tzumsteg1 <tzumsteg1@live.maryville.edu>
2019-05-12 11:06:50 -0500

    updated

--- dadjoke.py
+++ dadjoke.py
@@ -1,9 +1,12 @@
 import requests
+from colorama import init, Fore, Back
+
+init()
 
 response = requests.get(
     'https://icanhazdadjoke.com/',
     headers = {'Accept':'application/json'})
 
-print('Your dad joke: {0}'.format(response.json()['joke']))
+print(Back.BLUE + Fore.YELLOW + 'Your dad joke: {0}'.format(response.json()['joke']))
 
 input("Press ENTER to continue")
